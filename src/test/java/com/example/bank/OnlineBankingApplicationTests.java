29db6711013e36a5bc10c73d0596eca3e14b0a3c
Vivekdharan Muthupandi <vivekmuthupandi@gmail.com>
2026-01-28 17:24:11 +0530

    Update OnlineBankingApplicationTests.java

--- src/test/java/com/example/bank/OnlineBankingApplicationTests.java
+++ src/test/java/com/example/bank/OnlineBankingApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OnlineBankingApplicationTests {
+class OnlineBankingApplicationTests 
+{
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		
 	}
 
 }
